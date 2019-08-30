fcf75bd742ac61138704db933657ffbe2d04733c
pedrohenriquedevbr <p.lyma18@gmail.com>
2019-08-30 20:27:46 -0300

    Funçao para selecionar os dados do formulario personalizado desenvolvido

--- bancodehoras/relatorio/views.py
+++ bancodehoras/relatorio/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.conf import settings
 from relatorio import controller
+from datetime import datetime
 from core.models import *
 from movimentacao.views import seleciona_dados
 import os
@@ -11,9 +12,70 @@ import os
 # Create your views here.
 @login_required(login_url='login')
 def relatorio(request):
+    if request.method == 'POST':
+        data_inicial = request.POST.get('data_inicial')
+        data_final = request.POST.get('data_final')
+        status = request.POST.get('status')
+        andamento = request.POST.get('andamento')
+        colaborador = request.POST.get('colaborador')
+        forma_pagamento = request.POST.get('forma_pagamento')
+        tipo_movimentacao = request.POST.get('tipo_movimentacao')
+
+        inicio = datetime.strptime(data_inicial, '%Y-%m-%d')
+        fim = datetime.strptime(data_final, '%Y-%m-%d')
+        dados = Movimentacao.objects.filter(data_movimentacao__range=(inicio, fim))
+
+        if status != '0' and status != '': # Status
+            print('=========================================')
+            print('Aplicou o filtro de Status')
+            print('=========================================')
+            id_aux = int(status)
+            status = Status.objects.get(id=id_aux)
+            dados = dados.filter(status=status)
+
+        if andamento != '0' and andamento != '': # Andamento da solicitação
+            print('=========================================')
+            print('Aplicou o filtro de Andamento da solicitação')
+            print('=========================================')
+            id_aux = False
+            if id_aux == '1':
+                id_aux = True
+            dados = dados.filter(finalizado=id_aux)
+
+        if colaborador != '0' and colaborador != '': # colaborador especifico
+            print('=========================================')
+            print('Aplicou o filtro de Colaborador especifico')
+            print('=========================================')
+            id_aux = int(colaborador)
+            colaborador = Perfil.objects.get(id=id_aux)
+            dados = dados.filter(colaborador=colaborador)
+
+        if forma_pagamento != '0' and forma_pagamento != '': # Forma de pagamento
+            print('=========================================')
+            print('Aplicou o filtro de Forma de pagamento')
+            print('=========================================')
+            id_aux = int(forma_pagamento)
+            forma_pagamento = Status.objects.get(id=id_aux)
+            dados = dados.filter(forma_de_pagamento=forma_pagamento)
+
+        if tipo_movimentacao != '0' and tipo_movimentacao != '': # Tipo de movimentacao
+            print('=========================================')
+            print('Aplicou o filtro de tipo de movimentacao')
+            print('=========================================')
+            id_aux = False
+            if tipo_movimentacao == '1':
+                id_aux = True
+            dados = dados.filter(entrada=id_aux)
+
+        import pdb; pdb.set_trace()
+
     dados = seleciona_dados(request)
     return render(request, 'relatorio/relatorio.html', dados)
 
+
+def formata_dados_do_relatorio(dados, todas_as_movimentacoes=False, tipo_movimentacao=None):
+    pass
+    
 
 def gera_teste(request):
     pasta = 'relatorio/arquivos/'
