c95210a6e81570f71595d608cd34d71425922008
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-02-28 13:06:08 -0300

    feat: create admin class for models

--- bancodehoras/core/admin.py
+++ bancodehoras/core/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
 from core.models import *
+from django.contrib.auth.models import User
 
 
 class PerfilInline(admin.TabularInline):
     model = Perfil
 
 
+class MovimentacoesInline(admin.TabularInline):
+    model = Movimentacao
+
+
+class LogMovimentacaoInline(admin.TabularInline):
+    model = LogMovimentacao
+    readonly_fields = ['data', 'log', 'perfil_emissor', 'movimentacao']
+
+
 @admin.register(Setor)
 class SetorAdmin(admin.ModelAdmin):
     list_display = ['nome']
-    list_filter = ('nome',)
+    list_filter = ['nome']
     ordering = ['nome']
     search_fields = ['nome']
     inlines = [PerfilInline]
@@ -21,13 +31,65 @@ class PerfilAdmin(admin.ModelAdmin):
     list_filter = ['gerente', 'ch_primeira', 'ch_segunda']
     ordering = ['nome']
     search_fields = ['nome']
+    inlines = [MovimentacoesInline]
 
 
-# Register your models here.
-# admin.site.register(Setor, SetorAdmin)
-# admin.site.register(Perfil)
-# admin.site.register(Status)
-# admin.site.register(FormaDePagamento)
-# admin.site.register(Movimentacao)
-# admin.site.register(LogMovimentacao)
-# admin.site.register(Hash)
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'analise', 'autorizado']
+    list_filter = ['analise', 'autorizado']
+    ordering = ['nome']
+    search_fields = ['nome']
+
+
+@admin.register(FormaDePagamento)
+class FormaDePagamentoAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+    ordering = ['nome']
+    search_fields = ['nome']
+
+
+@admin.register(Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display = [
+        'data_cadastro',
+        'data_movimentacao',
+        'hora_inicial',
+        'hora_final',
+        'hora_total',
+        'motivo',
+        'entrada',
+        'finalizado',
+        'display_forma_de_pagamento',
+        'display_status',
+        'display_colaborador',
+        'display_setor'
+    ]
+    list_filter = [
+        'finalizado'
+    ]
+    ordering = ['data_cadastro']
+    search_fields = ['data_cadastro', 'data_movimentacao']
+    inlines = [LogMovimentacaoInline]
+
+
+@admin.register(LogMovimentacao)
+class LogMovimentacao(admin.ModelAdmin):
+    list_display = [
+        'data',
+        'log',
+        'display_perfil_emissor',
+        'display_movimentaca_data_cadastro',
+        'display_movimentaca_colaborador'
+    ]
+    ordering = [
+        'data'
+    ]
+    search_fields = ['data', 'log']
+
+
+@admin.register(Hash)
+class HashAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'chave', 'valor']
+    ordering = ['nome']
+    search_fields = ['nome', 'chave', 'valor']
--- bancodehoras/core/models.py
+++ bancodehoras/core/models.py
@@ -8,6 +8,10 @@ class Setor(models.Model):
     def __str__(self):
         return self.nome
 
+    class Meta:
+        verbose_name = 'Setor'
+        verbose_name_plural = 'Setores'
+
 
 class Perfil(models.Model):
     nome = models.CharField(max_length=100)
@@ -25,6 +29,10 @@ class Perfil(models.Model):
 
     display_setor.short_description = 'Setor'
 
+    class Meta:
+        verbose_name = 'Colaborador'
+        verbose_name_plural = 'Colaboradores'
+
 
 class Status(models.Model):
     nome = models.CharField(max_length=50)
@@ -34,12 +42,20 @@ class Status(models.Model):
     def __str__(self):
         return self.nome
 
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Status'
+
 
 class FormaDePagamento(models.Model):
     nome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
+
+    class Meta:
+        verbose_name = 'Forma de pagamento'
+        verbose_name_plural = 'Formas de pagamento'
 
 
 class Movimentacao(models.Model):
@@ -59,6 +75,35 @@ class Movimentacao(models.Model):
     def __str__(self):
         return str(self.data_cadastro) + ' - ' + self.colaborador.nome
 
+    def display_forma_de_pagamento(self):
+        if self.forma_de_pagamento is None:
+            return 'Não registrado'
+        return ''.join(self.forma_de_pagamento.nome)
+
+    def display_status(self):
+        if self.status is None:
+            return 'Não registrado'
+        return ''.join(self.status.nome)
+
+    def display_colaborador(self):
+        if self.status is None:
+            return 'Não registrado'
+        return ''.join(self.colaborador.nome)
+
+    def display_setor(self):
+        if self.colaborador.setor is None:
+            return 'Não registrado'
+        return self.colaborador.setor.nome
+
+    display_forma_de_pagamento.short_description = 'Pagamento'
+    display_status.short_description = 'Status'
+    display_colaborador.short_description = 'Colaborador'
+    display_colaborador.short_description = 'Setor'
+
+    class Meta:
+        verbose_name = 'Movimentação'
+        verbose_name_plural = 'Movimentações'
+    
 
 class LogMovimentacao(models.Model):
     data = models.DateTimeField(auto_now=True)
@@ -69,6 +114,35 @@ class LogMovimentacao(models.Model):
     def __str__(self):
         return str(self.data) + ' - ' + self.perfil_emissor.nome
 
+    def display_perfil_emissor(self):
+        if self.perfil_emissor is None:
+            return 'Perfil não localizado'
+        return self.perfil_emissor.nome
+
+    def display_movimentaca_data_cadastro(self):
+        if self.movimentacao is None:
+            return 'Movimentação não localizada'
+        else:
+            if self.movimentacao.data_cadastro is None:
+                return 'Data não registrada'
+        return self.movimentacao.data_cadastro
+
+    def display_movimentaca_colaborador(self):
+        if self.movimentacao is None:
+            return 'Movimentação não localizada'
+        else:
+            if self.movimentacao.data_cadastro is None:
+                return 'Colaborador não localizado'
+        return self.movimentacao.colaborador.nome
+
+    display_perfil_emissor.short_description = 'Colaborador'
+    display_movimentaca_data_cadastro.short_description = 'Data movimentação'
+    display_movimentaca_colaborador.short_description = 'criador da movimentação'
+
+    class Meta:
+        verbose_name = 'Log de movimentação'
+        verbose_name_plural = 'Log de movimentações'
+
 
 class Hash(models.Model):
     nome = models.CharField(max_length=50)
@@ -77,3 +151,7 @@ class Hash(models.Model):
 
     def __str__(self):
         return self.nome + ' - ' + self.chave + ' - ' + self.valor
+
+    class Meta:
+        verbose_name = 'Hash'
+        verbose_name_plural = 'Hash'
